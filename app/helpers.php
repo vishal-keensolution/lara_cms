d52adaf7d31dcef725e08bdc68928e5b44ce95e1
vishal-keensolution <81407076+vishal-keensolution@users.noreply.github.com>
2021-04-03 15:41:23 +0530

    seeder

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,11 @@
-<?php //die('here123');
+<?php 
+
+//----------------------Session And Role Check  Permission check
+
+function session_n_role_chk(){
+    
+}
+
+function check ($module, $id) {
+    return in_array($id, $_SESSION['user']['permissions'][$module]);
+  }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+     
+       \DB::table('tbl_user')->delete();
+        \DB::table('tbl_user')->insert(
+           [
+                "id"=>1,
+                "name"=>"Admin",
+                "email"=>"vishal.du123@gmail.com",
+                "phone"=>"9827530980",
+                "image"=>"",
+                "password"=>Hash::make("123456"),
+           ]
+        );
+        \DB::table('tbl_role')->delete();
+        \DB::table('tbl_role')->insert(
+            [
+                    "id"=>1,
+                    "name"=>"Super Admin"
+            ],
+            [
+                    "id"=>2,
+                    "name"=>"Admin"
+            ],
+            [
+                    "id"=>3,
+                    "name"=>"Editor"
+            ]
+        );
+        \DB::table('tbl_role_user')->delete();
+        \DB::table('tbl_role_user')->insert(
+            [
+                "id"=>1,
+                "userid"=>"1",
+                "roleid"=>"1"
+            ],
+            [
+                "id"=>2,
+                "userid"=>"1",
+                "roleid"=>"2"
+            ],
+            [
+                "id"=>3,
+                "userid"=>"1",
+                "roleid"=>"3"
+            ]
+        );
     }
 }
